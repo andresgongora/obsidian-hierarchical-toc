b24e69346555e8614f2e86b500a7bd42c4200582
Andres Gongora <mail@andresgongora.com>
2026-01-14 19:19:47 +0100

    Add release script
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+set -e
+set -o pipefail
+npm run build
+npm version patch
+# npm run release
+git push origin master --tags
+version=$(cat manifest.json | jq -r ".version")
+gh release create ${version} -F CHANGELOG.md manifest.json main.js styles.css
