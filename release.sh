d54eca6aa2fab6bcc05bd74d30f0a60f60e5286d
Andres Gongora <mail@andresgongora.com>
2026-01-22 09:02:50 +0100

    Update release script
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- release.sh
+++ release.sh
@@ -6,6 +6,6 @@ npm install
 npm run build
 npm version patch
 # npm run release
-git push origin master --tags
+git push origin main --tags
 version=$(cat manifest.json | jq -r ".version")
 gh release create ${version} -F CHANGELOG.md manifest.json main.js styles.css
